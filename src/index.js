ec093e8ae07b63c159eee3af67a1b02b2027f015
anafmt <ana.fmtavares@gmail.com>
2023-07-20 22:04:03 +0200

    Added Forecast JS

--- src/index.js
+++ src/index.js
@@ -40,6 +40,35 @@ if (minutes < 10) {
 let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = `${day}, ${date} ${month} ${hour}:${minutes}`;
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII="
+          alt=""
+         width="38"
+         />
+        <div class="weather-forecast-temperatures">
+         <span class="weather-forecast-temperature-max"> 18° </span>
+         <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+       </div>
+    `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function search(city) {
   let apiKey = "da34a047131b20d5faab7d8tfo459827";
   let units = "metric";
@@ -57,7 +86,13 @@ function changeCity(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", changeCity);
 
-// Week 5
+function getForecast(city) {
+  let apiKey = "da34a047131b20d5faab7d8tfo459827";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function showTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
@@ -79,6 +114,8 @@ function showTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.condition.description);
   celsiusTemperature = response.data.temperature.current;
+
+  getForecast(response.data.city);
 }
 
 function searchCity(city) {
@@ -95,6 +132,7 @@ function newLocation(position) {
   let units = "metric";
   let apiKey = "da34a047131b20d5faab7d8tfo459827";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=${units}`;
+
   function showTemperature(response) {
     let temperature = Math.round(response.data.temperature.current);
     let temperatureElement = document.querySelector("#temperature");
